f674b032ce7f397764a59a75ed2bb25c80ca207a
Youssef Hajji <hajji.youssef.personal@gmail.com>
2024-01-22 14:53:23 +0100

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,14 +4,14 @@ export default function Header(){
     return(
         <section className="header-section">
             <header className="header">
-                <img src="/images/white.png" alt=" logo image" className="header-logo" />
+                <img src="../../../images/white.png" alt=" logo image" className="header-logo" />
                 <nav className="header-nav">
                     <ul className="header-pages">
                         <a href="#home"><li className="header-list">Home</li></a>
                         <a href="#about"><li className="header-list">About us</li></a>
                         <a href="#services"><li className="header-list">Services</li></a>
                         <a href="#projects"><li className="header-list">Projects</li></a>
-                        <a href="#contact"><li className="header-list">Contact Us</li></a>
+                        <a href="#contact"><li className="header-list">Contact Me</li></a>
                     </ul>
                 </nav>
             </header>
