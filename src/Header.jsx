c916177565ef1ca29bf6b0174a3b6897123c3331
Youssef Hajji <hajji.youssef.personal@gmail.com>
2024-01-22 14:57:29 +0100

    image added

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <section className="header-section">
             <header className="header">
-                <img src="../../../images/white.png" alt=" logo image" className="header-logo" />
+                <img src="images/white.png" alt=" logo image" className="header-logo" />
                 <nav className="header-nav">
                     <ul className="header-pages">
                         <a href="#home"><li className="header-list">Home</li></a>
