4dfe455651c2d5ed7351dbaa0c889b67e64d3107
Youssef Hajji <hajji.youssef.personal@gmail.com>
2024-01-29 14:56:35 +0100

    changed some texts

--- src/About.jsx
+++ src/About.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function About(){
     return(
         <section className="about-section" id="about">
@@ -7,10 +8,20 @@ export default function About(){
                 <div className="about-desc">
                     <h3>Who We are ?</h3>
                     <p>
-                        Signature Construction has been a trusted name in 
-                        the construction industry. Our mission is to deliver unparalleled craftsmanship, 
-                        exceptional service, and lasting relationships. We are a team of dedicated 
-                        professionals driven by a shared commitment to excellence.
+                    Bienvenue chez Signature Construction, votre partenaire de confiance pour tous vos besoins en construction, 
+                    aménagement intérieur et extérieur, projets clé en main et travaux divers.
+                    Nous proposons une gamme complète de services, comprenant la construction, 
+                    l'architecture d'intérieur, la peinture, la pose de parquets, de papiers peints, 
+                    de carrelage mural et sol, ainsi que des travaux en plâtre simple et décoratif et tout type de travaux divers sur chantiers.
+                    Avec notre approche clé en main, nous prenons en charge l'ensemble du processus, de la conception à la réalisation, 
+                    pour vous offrir des solutions personnalisées et de haute qualité. 
+                    Que ce soit pour des projets résidentiels ou commerciaux, 
+                    nous sommes déterminés à surpasser vos attentes.
+                    Chez Signature Construction, notre engagement envers l'excellence se reflète dans chaque détail, 
+                    assurant la satisfaction de nos clients et la réussite de chaque projet. 
+                    Faites-nous confiance pour transformer vos idées en espaces exceptionnels, 
+                    où la qualité et l'innovation se marient harmonieusement.
+
                     </p>
                     <button className="btn button services">Check Our Services</button>
                 </div>
--- src/Services.jsx
+++ src/Services.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import './Services.css'
 import { MdConstruction } from "react-icons/md";
 
@@ -6,6 +7,7 @@ export default function Services(){
         <section className='services-section' id='services'>
             <h1 className="title">Our <span className="orange">Services</span></h1>
             <div className="row">
+                {/* <div className="col-sm-3"> */}
                     <div className="e-card playing col-sm-6 col-md-3 ">
                         <div className="image"></div>
                         <div className="wave"></div>
@@ -13,18 +15,21 @@ export default function Services(){
                         <div className="wave"></div>
                         <div className="services-container">
                             <MdConstruction className='icon'/>
-                            <h3 className='services-title'>Residential Construction</h3>
+                            <h3 className='services-title'>Conception 2D ET 3D</h3>
                             <p>
-                                Our expertise extends to crafting dream homes from the ground up. 
-                                Whether its a custom-built residence or a renovation project, 
-                                Signature Construction brings innovation, 
-                                quality, and attention to detail to every corner.
+                            Signature Construction vous offre une approche complète pour visualiser et planifier votre projet.
+                            À travers des plans détaillés en 2D et des modèles 3D réalistes, 
+                            nous vous offrons une perspective immersive de votre espace avant même le début de la construction. 
+                            Cela vous permet de personnaliser chaque détail et de garantir que la réalisation finale reflète parfaitement vos aspirations en matière d'aménagement intérieur et extérieur.
+
                             </p>
                         
                         </div>
                         
                     </div>
                    
+                {/* </div> */}
+                {/* <div className="col-sm-3"> */}
                 <div className="e-card playing col-sm-6 col-md-3 ">
                         <div className="image"></div>
                         <div className="wave"></div>
@@ -32,17 +37,18 @@ export default function Services(){
                         <div className="wave"></div>
                         <div className="services-container">
                             <MdConstruction className='icon'/>
-                            <h3 className='services-title'>Renovations and Remodeling</h3>
+                            <h3 className='services-title'>Projets clé en main</h3>
                             <p>
-                            Transform existing spaces into something extraordinary. 
-                            Our renovation and remodeling services breathe 
-                            new life into homes and businesses, 
-                            enhancing functionality and modernizing aesthetics.
+                            Signature Construction vous offre une solution tout-en-un pour la réalisation de vos projets de construction et d'aménagement. 
+                            De la conception initiale à la remise des clés, notre équipe gère tous les aspects du projet, garantissant une exécution transparente et de qualité. Vous pouvez avoir confiance en notre expertise pour transformer votre vision en réalité, 
+                            sans souci ni tracas.
                             </p>
                         
                         </div>
                         
                     </div>
+                {/* </div> */}
+                {/* <div className="col-sm-3"> */}
                 <div className="e-card playing col-sm-6 col-md-3">
                         <div className="image"></div>
                         <div className="wave"></div>
@@ -50,16 +56,22 @@ export default function Services(){
                         <div className="wave"></div>
                         <div className="services-container">
                             <MdConstruction className='icon'/>
-                            <h3 className='services-title'>Consultation and Design</h3>
+                            <h3 className='services-title'>Travaux sur mesure</h3>
                             <p>
-                            Collaborate with our experienced team during the initial stages of your project. 
-                            Our consultation and design services help define your vision, 
-                            ensuring a clear roadmap for a successful construction journey.
+                            Notre expertise en travaux sur mesure est la pour répondre à des besoins spécifiques en offrant des solutions personnalisées dans la construction, 
+                            la rénovation, et l'aménagement. À travers une consultation approfondie, 
+                            une conception sur mesure, la sélection minutieuse des matériaux, 
+                            une fabrication précise, et un contrôle de qualité strict, 
+                            nous créons des espaces uniques qui reflètent parfaitement votre style et vos exigences particulières. 
+                            Faites confiance à Signature Construction pour des travaux sur mesure qui incarnent votre vision de l'esthétique et de la fonctionnalité.
+
                             </p>
                         
                         </div>
                         
                     </div>
+                {/* </div> */}
+                {/* <div className="col-sm-3"> */}
                 <div className="e-card playing col-sm-6 col-md-3 ">
                         <div className="image"></div>
                         <div className="wave"></div>
@@ -67,12 +79,14 @@ export default function Services(){
                         <div className="wave"></div>
                         <div className="services-container">
                             <MdConstruction className='icon'/>
-                            <h3 className='services-title'>Residential Construction</h3>
+                            <h3 className='services-title'>Suivi de chantiers et assistance technique:</h3>
                             <p>
-                                Our expertise extends to crafting dream homes from the ground up. 
-                                Whether its a custom-built residence or a renovation project, 
-                                Signature Construction brings innovation, 
-                                quality, and attention to detail to every corner.
+                            Signature Construction assure une gestion experte du projet, 
+                            du début à la fin. Notre équipe supervise régulièrement le chantier, 
+                            oordonne les intervenants, contrôle les délais et budgets, et assure une qualité constante. 
+                            Nous offrons également une assistance technique, vous permettant de vivre l'évolution de votre projet en toute confiance. 
+                            Avec Signature Construction, votre projet est entre les mains de orofessionnels dédiés, 
+                            assurant une réalisation sans heurts et conforme à vos attentes.
                             </p>
                         
                         </div>
